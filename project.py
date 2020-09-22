6a7c7a2648d079cf2391b58b8c8c6f74d62e97b4
us-mishra79 <68797370+us-mishra79@users.noreply.github.com>
2020-09-22 12:18:20 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,105 @@
+nexts  =  True
+
+while nexts == True:
+    print(''' 
+    1- Aquaries
+    2- Pisces
+    3- Aries
+    4- Taurus
+    5- Gemini
+    6- Cancer
+    7- leo
+    8- Virgo
+    9- Libra
+    10- Scorpio
+    11- Sagittarius
+    12- Capricorn
+    
+    ''')
+    n = int(input('pickup yoursign number :\n'))
+    print(n)
+    if n == 1:
+        print('dfdf')
+    elif n == 2:
+        print('Pisces')
+    elif n == 3:
+        print('Aries ')
+    elif n == 4:
+        print('Taurus')
+    elif n == 5:
+        print('Gemini')
+    elif n == 6:
+        print('Cancer')
+    elif n == 7:
+        print('leo ')
+    elif n == 8:
+        print('Virgo')
+    elif n == 9:
+        print('Libra')
+    elif n == 10:
+        print('Scorpio')
+    elif n == 11:
+        print('Sagittarius')
+    elif n == 12:
+        print('Capricorn')
+    else:
+        print('please check your no. and try again')
+
+    next = True if input(' Shall we do it again?') == 'Y' else False
+
+
+
+
+
+
+
+
+# n = int(input('pickup yoursign number :\n'))
+# print(n)
+#
+# if n==1:
+#     print('dfdf')
+# elif n==2:
+#     print('Pisces')
+# elif n==3:
+#     print('Aries ')
+# elif n==4:
+#     print('Taurus')
+# elif n==5:
+#     print('Gemini')
+# elif n==6:
+#     print('Cancer')
+# elif n==7:
+#     print('leo ')
+# elif n==8:
+#     print('Virgo')
+# elif n==9:
+#     print('Libra')
+# elif n==10:
+#     print('Scorpio')
+# elif n==11:
+#     print('Sagittarius')
+# elif n==12:
+#     print('Capricorn')
+# else:
+#     print('please check your no. and try again')
+#
+#
+#
+# next = True if input(' Shall we do it again?') == Y else False
+#
+# # next = input("shall we do it again? ")
+# # if(next == Y):
+# #     next = True
+# # else:
+#     next = False
+
+
+
+ 
+ 
+   
+
+
+
+
